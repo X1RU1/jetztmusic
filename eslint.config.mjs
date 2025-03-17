9fb8a21c0beeab22dd3a1d1c8cee81dd79ba61ec
X1RU1 <evanievanna@gmail.com>
2025-03-17 15:12:25 +0100

    code rule configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import pluginPrettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: pluginPrettier
+    },
+    rules: {
+      "@typescript-eslint/no-var-requires": "off",
+      "import/no-commonjs": "off",
+      "import/no-dynamic-require": "off",
+      "import/no-unresolved": "off",
+      "prettier/prettier": "error"
+    }
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      "@typescript-eslint/no-var-requires": "off"
+    }
+  }
+]
