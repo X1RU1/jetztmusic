cb898ebbedab3f1d8ffb12fd27131a704129672b
X1RU1 <evanievanna@gmail.com>
2025-03-29 11:19:15 +0100

    code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react"
-import { useRoutes } from "react-router-dom"
+import React, { Suspense } from "react"
+import { Link, useRoutes } from "react-router-dom"
 import routes from "./router"
 
 function App() {
   return (
     <div className="App">
-      <div className="main">{useRoutes(routes)}</div>
+      <div className="nav">
+        <Link to="/discover">Explore Music</Link>
+        <Link to="/mine">My Music</Link>
+        <Link to="/focus">Following</Link>
+        <Link to="/download">Download</Link>
+      </div>
+      <Suspense fallback="">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
     </div>
   )
 }
