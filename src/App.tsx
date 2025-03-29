b56c73c07bba66f78c63a6f870922d2c9a65453b
X1RU1 <evanievanna@gmail.com>
2025-03-29 15:06:51 +0100

    redux useSelector state control

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React, { Suspense } from "react"
 import { Link, useRoutes } from "react-router-dom"
+import { shallowEqual } from "react-redux"
 import routes from "./router"
+import { useAppSelector } from "./store"
 
 function App() {
+  const { count, message } = useAppSelector(
+    (state) => ({
+      count: state.counter.count,
+      message: state.counter.message
+    }),
+    shallowEqual
+  )
+
   return (
     <div className="App">
       <div className="nav">
@@ -11,6 +21,8 @@ function App() {
         <Link to="/focus">Following</Link>
         <Link to="/download">Download</Link>
       </div>
+      <h2>Current Counter: {count}</h2>
+      <h2>Current Message: {message}</h2>
       <Suspense fallback="">
         <div className="main">{useRoutes(routes)}</div>
       </Suspense>
